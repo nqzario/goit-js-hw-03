8ff6e90cc7ca22d5e44079bfff0c581a54bea497
nqzario <159358436+elizabetlisa@users.noreply.github.com>
2024-05-02 10:51:55 +0300

    first commit

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,8 @@
-// function slugify(title) {
-//     const message = title.toLowerCase().split(' ').join('-');
-//     return message;
-// }
-// console.log(slugify("Arrays for begginers"));
-// console.log(slugify("English for developer"));
-// console.log(slugify("Ten secrets of JavaScript"));
-// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+function slugify(title) {
+    const message = title.toLowerCase().split(' ').join('-');
+    return message;
+}
+console.log(slugify("Arrays for begginers"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
